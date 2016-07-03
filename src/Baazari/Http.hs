6971154e581b75fd8657a653838756b614346c03
Nathan Lander <lander89@gmail.com>
2016-07-03 19:50:15 -0400

    make types for merchant and developer ids.

--- src/Baazari/Http.hs
+++ src/Baazari/Http.hs
@@ -7,6 +7,22 @@ import Data.Digest.OpenSSL.HMAC
 import Network.HTTP.Simple
 import Data.ByteString.Builder
 
+accessKeyId :: AccessKeyId
+
+marketplaceIds :: [MarketplaceId]
+marketplaceIds = [ "A2EUQ1WTGCTBG2"
+                 , "ATVPDKIKX0DER"
+                 , "A1AM78C64UM0Y8" ]
+
+sellerId :: SellerId
+
+secretKey :: SecretKey
+
+toParam :: ByteString
+        -> ByteString
+        -> (ByteString, Maybe ByteString)
+toParam name value = (name, Just value)
+
 renderEndpoint :: Endpoint -> ByteString
 renderEndpoint NorthAmerica =
   "mws.amazonservices.com"
@@ -34,24 +50,29 @@ makeQuery host =
 --          -> SignatureMethod
 --          -> Timestamp
 
-getEligibleShippingServices :: Endpoint
-                            -> AccessKeyId
-                            -> ShipmentRequestDetails
-                            -> AuthToken
-                            -> [MarketplaceId]
-                            -> SellerId
-                            -> SecretKey
-                            -> Request
-                            -> IO (Response ByteString)
-getEligibleShippingServices ep aId params token mIds sId key req =
-  do
-    signature <- fmap B64.encode $ hmac sha256 key unsigned
-  where unsigned = "POST\n"
-                <> renderEndpoint ep
-                <> "\n/\n"
-                <> renderAccessKeyId aId
-                <> "Action=GetEligibleShippingServices&"
-                <> renderShipmentRequestDetails params
-                <> renderAuthToken token
-                <> renderMarketplaceIds mIds
-                <> renderSellerId sId
+--getEligibleShippingServices :: Endpoint
+--                            -> AccessKeyId
+--                            -> ShipmentRequestDetails
+--                            -> [MarketplaceId]
+--                            -> SellerId
+--                            -> SecretKey
+--                            -> Request
+--                            -> IO (Response ByteString)
+--getEligibleShippingServices ep aId params token mIds sId key req = undefined
+--  do
+--    time <- renderTime <$> getCurrentTime
+--    signature <- fmap B64.encode $ hmac sha256 key $ unsigned time
+--  where unsigned time =
+--       "POST\n"
+--    <> renderEndpoint ep
+--    <> "\n/\n"
+--    <> renderAccessKeyId aId
+--    <> "Action=GetEligibleShippingServices&"
+--    <> renderShipmentRequestDetails params
+--    <> renderAuthToken token
+--    <> renderMarketplaceIds mIds
+--    <> renderSellerId sId
+--    <> "SignatureMethod=HmacSHA256&"
+--    <> "SignatureVersion=2&"
+--    <> "Timestamp=" <> time <> "&"
+--    <> "Version=2015-10-01"
